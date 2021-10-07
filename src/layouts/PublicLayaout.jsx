1621149a6392db4bd0cffa0ee0534c7d8ce8e917
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-06 22:17:24 -0500

    Corrige rutas

--- src/layouts/PublicLayaout.jsx
+++ src/layouts/PublicLayaout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Navbar from "components/Navbar";
-import Footer from "components/Footer";
+import Navbar from "../components/Footer";
+import Footer from "../components/Navbar";
 
 const PublicLayout = ({ children }) => {
   return (
