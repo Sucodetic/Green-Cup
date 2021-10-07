6caa1666ee7afbfda2f2108085a246ec2aa25a53
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-06 23:42:46 -0500

    Crea estados y funciones para dropdown

--- src/layouts/PublicLayaout.jsx
+++ src/layouts/PublicLayaout.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
+import Dropdown from "../components/Dropdown";
 
 const PublicLayout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", hideMenu);
+
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
+
   return (
     <div className="flex flex-col justify-between h-screen w-full">
       <Navbar toggle={toggle} />
