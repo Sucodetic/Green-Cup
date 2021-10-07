7f88a663d8f446845daaed672c27f86c1363aae3
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-06 22:20:16 -0500

    Configura router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import PublicLayout from "./layouts/PublicLayaout";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/styles.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path={["/admin", "/admin/productos", "admin/ventas"]}></Route>
+          <PublicLayout>
+            <Route path="/"></Route>
+          </PublicLayout>
+          <Route path={["/"]}>
+            <PublicLayout>
+              <Route path="/"></Route>
+            </PublicLayout>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
