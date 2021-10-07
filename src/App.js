fb844cb860b7fd2eea2c4e2891bf521e35baeff1
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-07 17:37:04 -0500

    Corrige rutas admin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import PublicLayout from "./layouts/PublicLayaout";
 import PrivateLayout from "./layouts/PrivateLayout";
 import Index from "./pages/Index";
-import Admin from "./pages/Index";
+import ProductosAdmin from "./pages/admin/Productos";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/styles.css";
 import "font-awesome/css/font-awesome.min.css";
@@ -11,11 +11,11 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path={["/admin"]}>
+          <Route path={["/admin/productos"]}>
             <PrivateLayout>
               <Switch>
-                <Route path="/admin">
-                  <Admin />
+                <Route path="/admin/productos">
+                  <ProductosAdmin />
                 </Route>
               </Switch>
             </PrivateLayout>
