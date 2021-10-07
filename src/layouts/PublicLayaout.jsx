00bdcdc33d6f964ec909a2f88f567bf330c79965
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-06 23:40:58 -0500

    Modifica fondo y quita scroll

--- src/layouts/PublicLayaout.jsx
+++ src/layouts/PublicLayaout.jsx
@@ -4,9 +4,10 @@ import Navbar from "../components/Navbar";
 
 const PublicLayout = ({ children }) => {
   return (
-    <div className="flex flex-col justify-between h-screen">
-      <Navbar />
-      <main className="h-full overflow-y-scroll bg-blue-400">{children}</main>
+    <div className="flex flex-col justify-between h-screen w-full">
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
+      <main className="h-full  bg-gray-300">{children}</main>
       <Footer />
     </div>
   );
