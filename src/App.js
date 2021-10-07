f33ade58746b55e730f5b37e68c0747f0b6f2f64
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-06 22:05:44 -0500

    Elimina texto innecesario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./styles/styles.css";
 
 function App() {
-  return (
-    <div className="App bg-yellow-700">
-      <h1 className="text-red-800">Hola</h1>
-    </div>
-  );
+  return <div className="App bg-yellow-700"></div>;
 }
 
 export default App;
