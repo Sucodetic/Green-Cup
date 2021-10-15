44f18d5ec15ca259d4c9319fc33c72fa38802db4
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-15 15:42:41 -0500

    Implementa header para enviar token a backed ventas

--- src/utils/ventas/api.js
+++ src/utils/ventas/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
+const getToken = () => {
+  return `Bearer ${localStorage.getItem("token")}`;
+};
+
 export const obtenerVenta = async (setProductos, setEjecutarConsulta) => {
-  const options = { method: "GET", url: "http://localhost:5000/ventas" };
+  const options = {
+    method: "GET",
+    url: "http://localhost:5000/ventas",
+    headers: {
+      Authorization: getToken(),
+    },
+  };
   await axios
     .request(options)
     .then(function (response) {
@@ -17,7 +27,7 @@ export const crearVenta = async (data, successCallback, errorCallBack) => {
   const options = {
     method: "POST",
     url: "http://localhost:5000/ventas",
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", Authorization: getToken() },
     data,
   };
 
@@ -28,7 +38,7 @@ export const editarVenta = async (id, data, successCallback, errorCallBack) => {
   const options = {
     method: "PATCH",
     url: `http://localhost:5000/ventas/${id}`,
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", Authorization: getToken() },
     data,
   };
 
@@ -39,7 +49,7 @@ export const eliminarVenta = async (id, successCallback, errorCallBack) => {
   const options = {
     method: "DELETE",
     url: `http://localhost:5000/ventas/${id}`,
-    headers: { "Content-Type": "application/json" },
+    headers: { "Content-Type": "application/json", Authorization: getToken() },
   };
 
   await axios.request(options).then(successCallback).catch(errorCallBack);
