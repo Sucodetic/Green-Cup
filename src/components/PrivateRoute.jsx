d697fac10076c103ac3cd8a2d94f3e4772991057
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-15 12:19:56 -0500

    Elimina llamados a funciones innecesarios

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 
 import { useAuth0 } from "@auth0/auth0-react";
 
 const PrivateRoute = ({children}) => {
-    const { user, isAuthenticated, isLoading } = useAuth0();
+    const { isAuthenticated, isLoading } = useAuth0();
 
     if(isLoading) return <div>Loading...</div>;
 
