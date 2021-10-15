9c7edd0e8f05df8574d2f561e59bd420c4a81c4c
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-15 11:01:54 -0500

    Implementa PrivateRoute

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import React, {useEffect} from 'react'
+
+import { useAuth0 } from "@auth0/auth0-react";
+
+const PrivateRoute = ({children}) => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if(isLoading) return <div>Loading...</div>;
+
+    return isAuthenticated ? <>{children}</>:<div className="text-2xl text-red-700">No puedes acceder a este sitio.</div> 
+}
+
+export default PrivateRoute
