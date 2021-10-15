a272f3453da88e142a6d229a6ef170d7706a9c6f
Jhonathan Pérez Ortega <jhperez.ortega@gmail.com>
2021-10-15 11:03:15 -0500

    COnfigura botón cerrar sesión

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ImagenLogo from "../media/logo.gif";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Sidebar = () => {
   return (
@@ -12,19 +13,25 @@ const Sidebar = () => {
         <Ruta icono="fa fa-cubes" ruta="/admin/productos" nombre="Productos" />
         <Ruta icono="fa fa-shopping-cart" ruta="/admin/ventas" nombre="Ventas" />
         <Ruta icono="fa fa-group" ruta="/admin/usuarios" nombre="Usuarios" />
-        <Ruta icono="	fa fa-sign-out" ruta="/" nombre="Cerrar sesión" />
+        <Ruta icono="  fa fa-sign-out" ruta="/" nombre="Cerrar sesión" />
       </div>
     </nav>
   );
 };
 
 const Ruta = ({ icono, ruta, nombre }) => {
+  const { logout } = useAuth0();
   return (
     <Link to={ruta}>
-      <button className="p-1 my-2  bg-green-600 hover:bg-green-900 flex w-full items-center text-white rounded-md">
+      {nombre === "Cerrar sesión" ? <button className="p-1 my-2  bg-green-600 hover:bg-green-900 flex w-full items-center text-white rounded-md" onClick={() => logout({ returnTo: window.location.origin })}>
         <i className={`${icono} w-10`} />
         {nombre}
       </button>
+        :
+       <button className="p-1 my-2  bg-green-600 hover:bg-green-900 flex w-full items-center text-white rounded-md">
+        <i className={`${icono} w-10`} />
+        {nombre}
+      </button>}
     </Link>
   );
 };
